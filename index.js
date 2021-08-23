ed96ae46c1dc0666ecc0066d89a8ecb3ec4ef97d
Sofia Bonakdar <sofia@bonakdar.se>
2021-08-23 23:43:34 +0200

    adding the first and the second tests

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-export function calculator(a, b) {
-  return a + b;
+const questions = [
+  {
+    id: 1,
+    title: "Material",
+    question: "What main material is your shoe made of?",
+    type: "multipleChoice",
+    answers: ["Leather", "Rubber", "Wood"],
+  },
+  {
+    id: 5,
+    title: "Sole material",
+    question: "What material is the sole made of?",
+    type: "multipleChoice",
+    answers: ["Leather", "Rubber", "Wood", "Metal"],
+  },
+];
+
+export function calculator(inputData) {
+  if (!inputData.answers) {
+    return {
+      question: questions[0],
+      code: "",
+      partial: true,
+    };
+  } else
+    return {
+      question: questions[1],
+      code: "6401",
+      partial: true,
+    };
 }
--- index.test.js
+++ index.test.js
@@ -1,5 +1,48 @@
-import { calculator } from './index';
+import { calculator } from "./index";
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(calculator(1, 2)).toBe(3);
+test("When no answers exist, then return first question", () => {
+  let inputData, result;
+
+  inputData = {
+    country: "SE",
+    system: "TARIC",
+  };
+
+  result = {
+    question: {
+      id: 1,
+      title: "Material",
+      question: "What main material is your shoe made of?",
+      type: "multipleChoice",
+      answers: ["Leather", "Rubber", "Wood"],
+    },
+    code: "",
+    partial: true,
+  };
+
+  expect(calculator(inputData)).toStrictEqual(result);
+});
+
+test("When the user has answered a question, return next question", () => {
+  let inputData, result;
+
+  inputData = {
+    country: "SE",
+    system: "TARIC",
+    answers: [{ questionId: 1, answer: 0 }],
+  };
+
+  result = {
+    question: {
+      id: 5,
+      title: "Sole material",
+      question: "What material is the sole made of?",
+      type: "multipleChoice",
+      answers: ["Leather", "Rubber", "Wood", "Metal"],
+    },
+    code: "6401",
+    partial: true,
+  };
+
+  expect(calculator(inputData)).toStrictEqual(result);
 });
