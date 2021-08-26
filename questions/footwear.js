bb080412e3a1f50322f7b1eb04a5c904c74ea017
Sofia Bonakdar <sofia@bonakdar.se>
2021-08-26 14:19:31 +0200

    question objects are added up to 6401

--- questions/footwear.js
+++ questions/footwear.js
@@ -1,51 +1,253 @@
 export const footwear = [
   {
-    key: 'footwearOrComponents',
-		title: `Footwear or Footwear Components?`,
-		text: `Are you importing footwear or components for footwear to EU input number in the table below
-		1, Footwear
-		2, Components & Parts of Footwear`,
-		answers: [
-			{
-				key: 'yes',
-				text: 'Footwear'
-			},
-			{
-				key: 'no',
-				text: 'Components & Parts of Footwear'
-			}
-		]
+    key: "footwearOrComponents",
+    title: `Footwear or Footwear Components?`,
+    text: `Are you importing footwear or components for footwear to EU input number in the table below:
+		1. Footwear
+		2. Components & Parts of Footwear`,
+    answers: [
+      {
+        key: "yes",
+        text: "Footwear",
+      },
+      {
+        key: "no",
+        text: "Components & Parts of Footwear",
+      },
+    ],
   },
-	{
-    key: 'upperType',
-		title: `What's the upper made of?`,
-		text: `Please input a number corresponding to the upper material:
-		1, Leather
-		2, Textile
-		3, Rubber
-		4, Plastic
-		5, Other`,
-		answers: [
-			{
-				key: 'leather',
-				text: 'Leather'
-			},
-			{
-				key: 'textile',
-				text: 'Textile'
-			},
-			{
-				key: 'rubber',
-				text: 'Rubber'
-			},
-			{
-				key: 'plastic',
-				text: 'Plastic'
-			},
-			{
-				key: 'other',
-				text: 'Other'
-			}
-		]
-  }
+  {
+    key: "upperType",
+    title: `What's the upper made of?`,
+    text: `Please input a number corresponding to the upper material:
+		1. Leather
+		2. Textile
+		3. Rubber
+		4. Plastic
+		5. Other`,
+    answers: [
+      {
+        key: "leather",
+        text: "Leather",
+      },
+      {
+        key: "textile",
+        text: "Textile",
+      },
+      {
+        key: "rubber",
+        text: "Rubber",
+      },
+      {
+        key: "plastic",
+        text: "Plastic",
+      },
+      {
+        key: "other",
+        text: "Other",
+      },
+    ],
+  },
+  {
+    key: "part",
+    title: `What kind of parts?`,
+    text: `Determine what kind of parts?
+		1. Leather
+		2. Textile
+		3. Rubber
+		4. Plastic
+		5. Other`,
+    answers: [
+      {
+        key: "uppers",
+        text: "Uppers and parts thereof, other stiffeners",
+      },
+      {
+        key: "heels",
+        text: "Outer soles and heels",
+      },
+      {
+        key: "withoutOuter",
+        text: "Assemblies of uppers affixed to inne soles or to outer soles but without outer soles",
+      },
+      {
+        key: "removable",
+        text: "Removable insoles and other removable accesories",
+      },
+      {
+        key: "outer",
+        text: "Outer soles",
+      },
+      {
+        key: "Gaiters",
+        text: "Gaiters, bone leather and similar articles and parts thereof",
+      },
+    ],
+  },
+  {
+    key: "sole",
+    title: `Sole material`,
+    text: `what kind of sole is it?
+		1. Leather
+		2. Immitation leather
+		3. Rubber
+		4. Plastic
+		6. Wood or cork
+		5. Other`,
+    answers: [
+      {
+        key: "leather",
+        text: "Immitation leather",
+      },
+      {
+        key: "textile",
+        text: "Textile",
+      },
+      {
+        key: "rubber",
+        text: "Rubber",
+      },
+      {
+        key: "plastic",
+        text: "Plastic",
+      },
+      {
+        key: "wood",
+        text: "Wood or cork",
+      },
+
+      {
+        key: "other",
+        text: "Other",
+      },
+    ],
+  },
+  {
+    key: "process",
+    title: `which process you used ?`,
+    text: `Please input a number corresponding to the process?
+	"cementing,
+      1. Moccasins,
+      2. Stich down and related,
+      3. Goodyear welted,
+      4. Stich and turn,
+      5. Pegged,
+      6. Opanka,
+      7. Norwegian,
+      8. Bologna(sacchetto),
+      9. blake and blake rapid,
+      10. Vulcanization,
+      11. Direct injection process,
+      12. Hand stiched,`,
+    answers: [
+      {
+        key: "moccasins",
+        text: "Moccasins",
+      },
+      {
+        key: "stichDown",
+        text: "Stich down and related",
+      },
+      {
+        key: "goodyear",
+        text: "Goodyear welted",
+      },
+      {
+        key: "stichTurn",
+        text: "Stich and turn",
+      },
+      {
+        key: "pegged",
+        text: "Pegged",
+      },
+
+      {
+        key: "opanka",
+        text: "Opanka",
+      },
+      {
+        key: "onorwegian",
+        text: "Norwegian",
+      },
+      {
+        key: "bologna",
+        text: "Bologna(sacchetto)",
+      },
+      {
+        key: "blake",
+        text: "Blake and blake rapid",
+      },
+      {
+        key: "vulcanization",
+        text: "Vulcanization",
+      },
+      {
+        key: "direct injection process",
+        text: "Direct injection process",
+      },
+      {
+        key: "handStiched",
+        text: "Hand stiched",
+      },
+    ],
+  },
+  {
+    key: "waterProof",
+    title: `Water Proof`,
+    text: `Is it water proof?
+		1. Yes
+		2. No
+	`,
+    answers: [
+      {
+        key: "yes",
+        text: "Yes",
+      },
+      {
+        key: "no",
+        text: "No",
+      },
+    ],
+  },
+  {
+    key: "toeCap",
+    title: `Metal toe cap`,
+    text: `Does the foorwear incorporate a metal toe cap?
+		1. Yes
+		2. No
+	`,
+    answers: [
+      {
+        key: "yes",
+        text: "Yes",
+      },
+      {
+        key: "no",
+        text: "No",
+      },
+    ],
+  },
+  {
+    key: "shaft",
+    title: `Length of the shafts`,
+    text: `Determine the length shafts?
+		1. Shafts that cover the ankle but not the knee
+		2. Shafts that cover the knee
+		3. Other
+	`,
+    answers: [
+      {
+        key: "ankle",
+        text: "Shafts that cover the ankle but not the knee",
+      },
+      {
+        key: "knee",
+        text: "Shafts that cover the knee",
+      },
+      {
+        key: "other",
+        text: "Other",
+      },
+    ],
+  },
 ];
