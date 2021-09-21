076b4dc7ba97c29e8e755e36148d43cb5f720b03
Sebastian Warvsten <sebastian.warvsten@gmail.com>
2021-09-21 11:45:56 +0200

    New questions-file added

---
+++ questions/footwearNew.js
@@ -0,0 +1,371 @@
+export const footwearNew = [
+  {
+    key: 'country',
+    title: 'What country are you shipping to',
+    text: '',
+    type: 'single',
+    answer: [
+      {
+        key: 'eu',
+        text: 'EU',
+      },
+      {
+        key: 'other',
+        text: 'Other',
+      }
+    ],
+    originalQuestionKey: ''
+  },
+  {
+    key: 'footWearOrComponents',
+    title: 'Are you importing footwear or footwear components',
+    text: '',
+    type: 'single',
+    answer: [
+      {
+        key: 'footwear',
+        text: 'Footwear',
+      },
+      {
+        key: 'components',
+        text: 'Components',
+      }
+    ],
+    originalQuestionKey: 'footwearOrComponents'
+  },
+  {
+    key: 'upperType',
+    title: 'What is the upper made of',
+    text: '',
+    type: 'single',
+    answer: [
+      {
+        key: 'leather',
+        text: 'Leather',
+      },
+      {
+        key: 'textile',
+        text: 'Textile',
+      },
+      {
+        key: 'rubber',
+        text: 'Rubber',
+      },
+      {
+        key: 'plastic',
+        text: 'Plastic',
+      },
+      {
+        key: 'other',
+        text: 'Other',
+      },
+    ],
+    originalQuestionKey: 'upperType'
+  },
+  {
+    key: 'soleType',
+    title: 'What is the sole made of',
+    text: '',
+    type: 'single',
+    answer: [
+      {
+        key: 'leather',
+        text: 'Leather',
+      },
+      {
+        key: 'immitationLeather',
+        text: 'ImmitationLeather',
+      },
+      {
+        key: 'rubber',
+        text: 'Rubber',
+      },
+      {
+        key: 'plastic',
+        text: 'Plastic',
+      },
+      {
+        key: 'wood',
+        text: 'Wood or cork',
+      },
+      {
+        key: 'other',
+        text: 'Other',
+      },
+    ],
+    originalQuestionKey: 'sole'
+  },
+  {
+    key: 'qualities',
+    title: 'What are the qualities of your shoe',
+    text: '',
+    type: 'multi',
+    answer: [
+      {
+        key: 'sports',
+        text: 'Sports footwear',
+        originalQuestionKey: 'sports'
+      },
+      {
+        key: 'slippers',
+        text: 'Slippers',
+        originalQuestionKey: 'slippers'
+      },
+      {
+        key: 'waterProof',
+        text: 'Water Proof',
+        originalQuestionKey: 'waterProof'
+      },
+      {
+        key: 'metalToeCap',
+        text: 'Metal Toe Cap',
+        originalQuestionKey: 'toeCap'
+      },
+      {
+        key: 'winterSports',
+        text: 'Winter Sports',
+        originalQuestionKey: 'winterSports'
+      }
+    ],
+  },
+  {
+    key: 'skiBoots',
+    title: 'What kind of winter sports footwear is it',
+    text: '',
+    type: 'single',
+    answer: [
+      {
+        key: 'skiBoots',
+        text: 'Ski Boots',
+      },
+      {
+        key: 'snowboardBoots',
+        text: 'Snowboard Boots',
+      },
+      {
+        key: 'other',
+        text: 'Other',
+      }
+    ],
+    originalQuestionKey: 'skiBoots'
+  },
+  {
+    key: 'process',
+    title: 'Which process is use',
+    text: '',
+    type: 'single',
+    answers: [
+      {
+        key: "moccasins",
+        text: "Moccasins",
+      },
+      {
+        key: "stichDown",
+        text: "Stich down and related",
+      },
+      {
+        key: "goodyear",
+        text: "Goodyear welted",
+      },
+      {
+        key: "stichTurn",
+        text: "Stich and turn",
+      },
+      {
+        key: "pegged",
+        text: "Pegged",
+      },
+
+      {
+        key: "opanka",
+        text: "Opanka",
+      },
+      {
+        key: "norwegian",
+        text: "Norwegian",
+      },
+      {
+        key: "bologna",
+        text: "Bologna(sacchetto)",
+      },
+      {
+        key: "blake",
+        text: "Blake and blake rapid",
+      },
+      {
+        key: "vulcanization",
+        text: "Vulcanization",
+      },
+      {
+        key: "direct injection process",
+        text: "Direct injection process",
+      },
+      {
+        key: "handStiched",
+        text: "Hand stiched",
+      },
+    ],
+    originalQuestionKey: 'process'
+  },
+  {
+    key: 'shaft',
+    title: 'What part does the shaft cover',
+    text: '',
+    type: 'single',
+    answer: [
+      {
+        key: 'knee',
+        text: 'Knee',
+      },
+      {
+        key: 'ankle',
+        text: 'Ankle',
+      },
+      {
+        key: 'other',
+        text: 'Other',
+      }
+    ],
+    originalQuestionKey: 'shaft'
+  },
+  {
+    key: 'sandal',
+    title: 'Is the shoe a sandal',
+    text: '',
+    type: 'single',
+    answer: [
+      {
+        key: 'sandal',
+        text: 'Sandal',
+      },
+      {
+        key: 'other',
+        text: 'Other',
+      }
+    ],
+    originalQuestionKey: 'vamp'
+  },
+  {
+    key: 'gender',
+    title: 'What gender is the boot for',
+    text: '',
+    type: 'single',
+    answer: [
+      {
+        key: 'man',
+        text: 'Man',
+      },
+      {
+        key: 'woman',
+        text: 'Woman',
+      },
+      {
+        key: 'unisex/other',
+        text: 'Unisex/Other',
+      }
+    ],
+    originalQuestionKey: 'genderType'
+  },
+  {
+    key: 'kidsShoe',
+    title: 'Is it a kids shoe',
+    text: '',
+    type: 'single',
+    answer: [
+      {
+        key: 'yes',
+        text: 'Yes',
+      },
+      {
+        key: 'no',
+        text: 'No',
+      },
+    ],
+    originalQuestionKey: 'lengthOfInsole'
+  },
+  {
+    key: 'heightOfSoleAndHeel',
+    title: 'Is the height of your sole and heal more then 3cm',
+    text: '',
+    type: 'single',
+    answer: [
+      {
+        key: 'yes',
+        text: 'Yes',
+      },
+      {
+        key: 'no',
+        text: 'No',
+      },
+    ],
+    originalQuestionKey: 'heightOfSoleAndHeel'
+  },
+  {
+    key: 'madeOnBase',
+    title: 'Is the footwear made on a base or plattform made of wood not having an inner sole',
+    text: '',
+    type: 'single',
+    answer: [
+      {
+        key: 'yes',
+        text: 'Yes',
+      },
+      {
+        key: 'no',
+        text: 'No',
+      },
+    ],
+    originalQuestionKey: 'madeOnBase'
+  },
+  {
+    key: 'handmade',
+    title: 'Is the footwear handmade',
+    text: '',
+    type: 'single',
+    answer: [
+      {
+        key: 'yes',
+        text: 'Yes',
+      },
+      {
+        key: 'no',
+        text: 'No',
+      },
+    ],
+    originalQuestionKey: 'handmade'
+  },
+  {
+    key: 'leatherStraps',
+    title: 'Does the footwear have a leather straps across the instep and around the big toe',
+    text: '',
+    type: 'single',
+    answer: [
+      {
+        key: 'yes',
+        text: 'Yes',
+      },
+      {
+        key: 'no',
+        text: 'No',
+      },
+    ],
+    originalQuestionKey: 'leatherStraps'
+  },
+  {
+    key: 'upperStrapsOrThongs',
+    title: 'Does the footwear have upper straps or thongs assembled to the sole by the means of plugs',
+    text: '',
+    type: 'single',
+    answer: [
+      {
+        key: 'yes',
+        text: 'Yes',
+      },
+      {
+        key: 'no',
+        text: 'No',
+      },
+    ],
+    originalQuestionKey: 'upperStrapsOrThongs'
+  },
+]
